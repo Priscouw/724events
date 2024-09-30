f059f51b16c1ef116e4c06ecfac0b821af11aa64
Priscouw <priscouw@gmail.com>
2024-09-30 12:15:16 +0200

    <refactor> move div SlideCard__paginationContainer  move div SlideCard__paginationContainer from first map to remove duplicate pagination

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -24,36 +24,34 @@ const Slider = () => {
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <div key={event.title}>
-
-          <div
-            className={`SlideCard SlideCard--${index === idx ? "display" : "hide"
-              }`}
-          >
-            <img src={event.cover} alt="forum" />
-            <div className="SlideCard__descriptionContainer">
-              <div className="SlideCard__description">
-                <h3>{event.title}</h3>
-                <p>{event.description}</p>
-                <div>{getMonth(new Date(event.date))}</div>
-              </div>
-            </div>
-          </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((evt, radioIdx) => (
-                <input
-                  key={evt.title}
-                  type="radio"
-                  name="radio-button"
-                  checked={index === radioIdx}
-                  readOnly
-                />
-              ))}
+        <div key={event.title}
+          className={`SlideCard SlideCard--${index === idx ? "display" : "hide"
+            }`}
+        >
+          <img src={event.cover} alt="forum" />
+          <div className="SlideCard__descriptionContainer">
+            <div className="SlideCard__description">
+              <h3>{event.title}</h3>
+              <p>{event.description}</p>
+              <div>{getMonth(new Date(event.date))}</div>
             </div>
           </div>
         </div>
-      ))}
+
+      ))};
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((evt, radioIdx) => (
+            <input
+              key={evt.title}
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              readOnly
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
